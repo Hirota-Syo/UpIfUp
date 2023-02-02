490d010b09d932dab54dcc22bb64f088471fa4d6
Hirota-Syo <hirota.syo.0327@gmail.com>
2023-02-02 18:54:57 +0900

    pass when type is int

---
+++ CCsv.hpp
@@ -0,0 +1,24 @@
+#ifndef   CCSV_HPP
+#define   CCSV_HPP
+
+#include <string>
+#include <fstream>
+#include <vector>
+#include <utility> // std::pair
+#include <stdexcept> // std::runtime_error
+#include <sstream> // std::stringstream
+
+class CCsv
+{
+	// variable
+private:
+	int m_value;
+
+	// accessor
+public:
+	void write_csv(std::string filename, std::vector<std::pair<std::string, std::vector<int>>> dataset);
+	std::vector<std::pair<std::string, std::vector<int>>> read_csv(std::string filename);
+	int main();
+};
+
+#endif // CCSV_HPP
---
+++ Main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+#include "CCsv.hpp"
+
+using namespace std;
+
+int main() {
+	CCsv csv;
+	
+	// read a csv file
+	vector<pair<string, vector<int>>> CsvRd;
+	CsvRd = csv.read_csv("Input/FileInfo.csv");
+
+	// 
+
+	// get a second row
+	string head0 = CsvRd.at(0).first;
+	string head1 = CsvRd.at(1).first;
+	//string C_0_0 = CsvRd.at(0).secoond.at(0);
+	//string C_1_0 = CsvRd.at(0).second.at(1);
+	//string C_0_2 = CsvRd.at(2).second.at(0);
+	//string C_1_2 = CsvRd.at(2).second.at(1);
+
+	cout << head0 << endl;
+	cout << head1 << endl;
+	//cout << C_0_0 << endl;
+	//cout << C_1_0 << endl;
+	//cout << C_0_2 << endl;
+	//cout << C_1_2 << endl;
+
+	// 
+		return 0;
+}
+
+//g(f(x)) z;
+//x = {g^(-1)}({f^(-1)}(z)) = z.g^(-1).f^(-1)
